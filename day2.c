feab78bacb282fe9b054417c3f616d63ef9c3971
Deepnam Bhaatt <bhattdeepnam@gmail.com>
2026-08-14 02:23:21 +0530

    Day 2 -2

--- day2.c
+++ day2.c
@@ -1,18 +1,21 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <unistd.h>
 pthread_rwlock_t lock;
 int counter = 1;
-void *write(void *arg) {
-  for (int i = 0; i < 10; i++) {
+void *my_write(void *arg) {
+  for (int i = 1; i < 10; i++) {
     pthread_rwlock_wrlock(&lock);
     counter++;
     pthread_rwlock_unlock(&lock);
   }
   return NULL;
 }
-void *read(void *arg) {
+void *my_read(void *arg) {
   pthread_rwlock_rdlock(&lock);
-  printf("The value of counter is %d ", counter);
+  printf("Thread entered\n");
+  printf("The value of counter is %d\n ", counter);
+  sleep(1);
   pthread_rwlock_unlock(&lock);
   return NULL;
 }
@@ -21,9 +24,9 @@ int main() {
   pthread_t reader1;
   pthread_t reader2;
   pthread_rwlock_init(&lock, NULL);
-  pthread_create(&writer, NULL, write, NULL);
-  pthread_create(&reader1, NULL, read, NULL);
-  pthread_create(&reader2, NULL, read, NULL);
+  pthread_create(&writer, NULL, my_write, NULL);
+  pthread_create(&reader1, NULL, my_read, NULL);
+  pthread_create(&reader2, NULL, my_read, NULL);
   pthread_join(writer, NULL);
   pthread_join(reader1, NULL);
   pthread_join(reader2, NULL);
