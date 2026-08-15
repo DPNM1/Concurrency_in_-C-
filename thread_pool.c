c501f40f33b78a9de50e918c32531bee605f21e9
Deepnam Bhaatt <bhattdeepnam@gmail.com>
2026-08-15 22:00:06 +0530

    Day 3 - Thread Pool V1

--- thread_pool.c
+++ thread_pool.c
@@ -1,107 +1,187 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
-#define QUEUE_SIZE 5
+
+#define QUEUE_SIZE 20
+#define JOB_COUNT 10
+#define WORKER_COUNT 3
+
 typedef struct {
   void (*function)(void *);
   void *arg;
 } Jobs;
+
 typedef struct {
   Jobs jobs[QUEUE_SIZE];
+
   int head;
   int tail;
   int count;
+  int shutdown;
   pthread_mutex_t lock;
   pthread_cond_t condition;
 } Queue;
+
 typedef struct {
   Queue *queue;
-  Jobs job;
+  Jobs *jobs;
+  int job_count;
 } Producer;
 
 typedef struct {
   Queue *queue;
 } Worker;
 
-void print_me(void *arg) {
-  int *a = arg;
-  printf("The value is %d\n", *a);
+// ---------------- JOB FUNCTION ----------------
+
+void process(void *arg) {
+  int *number = arg;
+
+  printf("Processing job %d\n", *number);
+
+  sleep(1);
+
+  printf("Finished job %d\n", *number);
 }
 
-void increment(void *arg) {
-  int *a = arg;
-  *a += 1;
-  printf("The incremented value is %d\n", *a);
-}
-
-void queue_push(Queue *queue, Jobs job) {
-  pthread_mutex_lock(&queue->lock);
-  if (queue->count < QUEUE_SIZE) {
-    queue->jobs[queue->tail] = job;
-    queue->tail = (queue->tail + 1) % QUEUE_SIZE;
-    queue->count++;
-    printf("JOB PUSHED SUCCESSFULLY!\n");
-  } else {
-    printf("MAX JOBS IN QUEUE");
-  }
-  pthread_mutex_unlock(&queue->lock);
-  pthread_cond_signal(&queue->condition);
-}
-
-Jobs queue_pop(Queue *queue) {
-  pthread_mutex_lock(&queue->lock);
-  while (queue->count == 0) {
-    pthread_cond_wait(&queue->condition, &queue->lock);
-  }
-  Jobs job = queue->jobs[queue->head];
-  queue->head = (queue->head + 1) % QUEUE_SIZE;
-  queue->count--;
-  pthread_mutex_unlock(&queue->lock);
-  return job;
-}
+// ---------------- QUEUE ----------------
 
 void queue_init(Queue *queue) {
   queue->head = 0;
   queue->tail = 0;
   queue->count = 0;
+  queue->shutdown = 0;
+
   pthread_mutex_init(&queue->lock, NULL);
   pthread_cond_init(&queue->condition, NULL);
 }
 
-void *Produce(void *arg) {
-  Producer *args = arg;
-  sleep(1);
-  queue_push(args->queue, args->job);
+void queue_push(Queue *queue, Jobs job) {
+  pthread_mutex_lock(&queue->lock);
+
+  if (queue->count < QUEUE_SIZE) {
+
+    queue->jobs[queue->tail] = job;
+
+    queue->tail = (queue->tail + 1) % QUEUE_SIZE;
+
+    queue->count++;
+
+    printf("Job pushed\n");
+  }
+  pthread_mutex_unlock(&queue->lock);
+  pthread_cond_broadcast(&queue->condition);
+}
+
+int queue_pop(Queue *queue, Jobs *job) {
+  pthread_mutex_lock(&queue->lock);
+
+  while (queue->count == 0 && !queue->shutdown) {
+    pthread_cond_wait(&queue->condition, &queue->lock);
+  }
+
+  // No jobs remain AND shutdown was requested
+  if (queue->count == 0 && queue->shutdown) {
+    pthread_mutex_unlock(&queue->lock);
+    return 0;
+  }
+
+  // We have a real job
+  *job = queue->jobs[queue->head];
+
+  queue->head = (queue->head + 1) % QUEUE_SIZE;
+
+  queue->count--;
+
+  pthread_mutex_unlock(&queue->lock);
+
+  return 1;
+}
+// ---------------- PRODUCER ----------------
+
+void *produce(void *arg) {
+  Producer *producer = arg;
+
+  for (int i = 0; i < producer->job_count; i++) {
+
+    queue_push(producer->queue, producer->jobs[i]);
+  }
+  producer->queue->shutdown = 1;
   return NULL;
 }
 
-void *Work(void *arg) {
-  Worker *args = arg;
-  Jobs work = queue_pop(args->queue);
-  work.function(work.arg);
-  printf("Work Completed");
+// ---------------- WORKER ----------------
+
+void *work(void *arg) {
+  Worker *worker = arg;
+
+  while (1) {
+
+    Jobs job;
+
+    int result = queue_pop(worker->queue, &job);
+
+    if (result == 0) {
+      break;
+    }
+
+    job.function(job.arg);
+  }
+
   return NULL;
 }
+
+// ---------------- MAIN ----------------
 
 int main() {
-  Jobs job1, job2;
-  Queue q1;
-  queue_init(&q1);
-  int x = 10;
-  job1.function = print_me;
-  job1.arg = &x;
-  job2.function = increment;
-  job2.arg = &x;
-  Producer p;
-  p.queue = &q1;
-  p.job = job2;
-  Worker w;
-  w.queue = &q1;
-  pthread_t producer;
-  pthread_t worker1, worker2;
-  pthread_create(&producer, NULL, &Produce, &p);
-  pthread_create(&worker1, NULL, &Work, &w);
-  pthread_join(producer, NULL);
-  pthread_join(worker1, NULL);
+  Queue queue;
+
+  queue_init(&queue);
+
+  // Our actual data
+  int numbers[JOB_COUNT];
+
+  // Our 10 Jobs
+  Jobs jobs[JOB_COUNT];
+
+  // Create the 10 jobs
+  for (int i = 0; i < JOB_COUNT; i++) {
+
+    numbers[i] = i + 1;
+
+    jobs[i].function = process;
+
+    jobs[i].arg = &numbers[i];
+  }
+
+  // Producer
+  Producer producer;
+
+  producer.queue = &queue;
+  producer.jobs = jobs;
+  producer.job_count = JOB_COUNT;
+
+  // Workers
+  Worker worker;
+
+  worker.queue = &queue;
+
+  pthread_t producer_thread;
+
+  pthread_t worker_threads[WORKER_COUNT];
+
+  // Create workers
+  for (int i = 0; i < WORKER_COUNT; i++) {
+
+    pthread_create(&worker_threads[i], NULL, work, &worker);
+  }
+
+  // Create producer
+  pthread_create(&producer_thread, NULL, produce, &producer);
+
+  pthread_join(producer_thread, NULL);
+  for (int i = 0; i < WORKER_COUNT; i++) {
+    pthread_join(worker_threads[i], NULL);
+  }
   return 0;
 }
