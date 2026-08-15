00968397ceae85d4169b4e2e33a827e0e7e9b61f
Deepnam Bhaatt <bhattdeepnam@gmail.com>
2026-08-15 16:12:09 +0530

    Day 3 - Thread Pool V0.1

--- thread_pool.c
+++ thread_pool.c
@@ -1,5 +1,6 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <unistd.h>
 #define QUEUE_SIZE 5
 typedef struct {
   void (*function)(void *);
@@ -10,6 +11,8 @@ typedef struct {
   int head;
   int tail;
   int count;
+  pthread_mutex_t lock;
+  pthread_cond_t condition;
 } Queue;
 typedef struct {
   Queue *queue;
@@ -32,6 +35,7 @@ void increment(void *arg) {
 }
 
 void queue_push(Queue *queue, Jobs job) {
+  pthread_mutex_lock(&queue->lock);
   if (queue->count < QUEUE_SIZE) {
     queue->jobs[queue->tail] = job;
     queue->tail = (queue->tail + 1) % QUEUE_SIZE;
@@ -40,27 +44,33 @@ void queue_push(Queue *queue, Jobs job) {
   } else {
     printf("MAX JOBS IN QUEUE");
   }
+  pthread_mutex_unlock(&queue->lock);
+  pthread_cond_signal(&queue->condition);
 }
 
 Jobs queue_pop(Queue *queue) {
-  if (queue->count != 0) {
-    Jobs job = queue->jobs[queue->head];
-    queue->head = (queue->head + 1) % QUEUE_SIZE;
-    queue->count--;
-    return job;
-  } else {
-    printf("NO EXISTING JOB");
+  pthread_mutex_lock(&queue->lock);
+  while (queue->count == 0) {
+    pthread_cond_wait(&queue->condition, &queue->lock);
   }
+  Jobs job = queue->jobs[queue->head];
+  queue->head = (queue->head + 1) % QUEUE_SIZE;
+  queue->count--;
+  pthread_mutex_unlock(&queue->lock);
+  return job;
 }
 
 void queue_init(Queue *queue) {
   queue->head = 0;
   queue->tail = 0;
   queue->count = 0;
+  pthread_mutex_init(&queue->lock, NULL);
+  pthread_cond_init(&queue->condition, NULL);
 }
 
 void *Produce(void *arg) {
   Producer *args = arg;
+  sleep(1);
   queue_push(args->queue, args->job);
   return NULL;
 }
